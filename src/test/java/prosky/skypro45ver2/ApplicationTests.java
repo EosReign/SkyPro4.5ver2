a1571ab1ef9934e3ca0711560568020986fee126
EosReign <vsevolod_sol@mail.ru>
2022-12-01 15:43:11 +0300

    Initial commit

---
+++ src/test/java/prosky/skypro45ver2/ApplicationTests.java
@@ -0,0 +1,13 @@
+package prosky.skypro45ver2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
